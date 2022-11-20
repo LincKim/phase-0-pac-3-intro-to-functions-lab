f40351577f5ebd745632d9d0e3928ca12ef1e616
LincKim <lincolnkimutai@gmail.com>
2022-11-20 20:07:54 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase("HELLO");
+}
+function logShout(string) {
+   console.log("hello".toUpperCase());
+
+const spy = "hello"
+    
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase);
+        const spy = "hello"
+        console.log("hello");
+    
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase(string) === string) {
+        return ("I can't hear you!");
+    }
+    else if (string.toUpperCase(string) === string){
+        return ("YES INDEED!");
+    }
+    else if (string){
+        return ("I would love to!");
+    }
+}
+sayHiToHeadphonedRoommate("hello");
+sayHiToHeadphonedRoommate("HELLO");
+sayHiToHeadphonedRoommate("Let's have dinner together!");
+
